893cf4f9be5c24d67143cf8dace04c9d652af3a1
Matt Troutman <git@trtmn.com>
2025-07-22 13:45:31 -0500

    chore: bump version to 0.3.1 and update documentation
    - Updated the version in `pyproject.toml`, `__init__.py`, and various documentation files to reflect the new version 0.3.1.
    - Changed the required Python version from 3.10 to 3.11 in `pyproject.toml`.
    - Enhanced the documentation to ensure consistency with the new versioning.

--- src/testrail_api_module/__init__.py
+++ src/testrail_api_module/__init__.py
@@ -18,7 +18,7 @@ Attributes:
 import os
 from typing import Optional
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 """The version of the module, used for compatibility checks and logging."""
 __author__ = 'Matt Troutman, Christian Thompson, Andrew Tipper'
 
